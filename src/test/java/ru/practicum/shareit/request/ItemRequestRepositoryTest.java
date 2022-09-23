5fd755847e5945dad0c89ad80cc9ad3e3c99923c
Izenkyt <smurfetter21@yandex.ru>
2022-09-23 21:29:20 +0300

    fix: merge test

--- src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
@@ -25,7 +24,7 @@ public class ItemRequestRepositoryTest {
     private ItemRequestRepository itemRequestRepository;
 
     @Test
-    void findAllByRequesterId() {
+    void findAllByRequesterIdAndNotId() {
         var requester1 = new User();
         requester1.setEmail("user1@mail.com");
         requester1.setName("User1Name");
@@ -55,57 +54,17 @@ public class ItemRequestRepositoryTest {
         itemRequest3.setDescription("ItemRequest3Description");
         itemRequest3.setCreated(LocalDateTime.now());
 
-        tem.persist(requester1);
         tem.persist(requester2);
         tem.persist(item);
         tem.persist(itemRequest1);
         tem.persist(itemRequest2);
         tem.persist(itemRequest3);
 
-        var itemRequests = itemRequestRepository.findAllByRequesterId(1L, Sort.unsorted());
-        assertThat(itemRequests.size(), is(2));
-    }
+        var itemRequestsByRequesterId = itemRequestRepository.findAllByRequesterId(1L, Sort.unsorted());
+        assertThat(itemRequestsByRequesterId.size(), is(2));
 
-    @Test
-    void findAllByRequesterIdNot() {
-        var requester1 = new User();
-        requester1.setEmail("user1@mail.com");
-        requester1.setName("User1Name");
-
-        var requester2 = new User();
-        requester2.setEmail("user2@mail.com");
-        requester2.setName("User2Name");
-
-        var item = new Item();
-        item.setName("ItemName");
-        item.setDescription("ItemDescription");
-        item.setAvailable(true);
-        item.setOwner(requester1);
-
-        var itemRequest1 = new ItemRequest();
-        itemRequest1.setRequester(requester1);
-        itemRequest1.setDescription("ItemRequest1Description");
-        itemRequest1.setCreated(LocalDateTime.now());
-
-        var itemRequest2 = new ItemRequest();
-        itemRequest2.setRequester(requester1);
-        itemRequest2.setDescription("ItemRequest2Description");
-        itemRequest2.setCreated(LocalDateTime.now());
-
-        var itemRequest3 = new ItemRequest();
-        itemRequest3.setRequester(requester2);
-        itemRequest3.setDescription("ItemRequest3Description");
-        itemRequest3.setCreated(LocalDateTime.now());
-
-        tem.persist(requester1);
-        tem.persist(requester2);
-        tem.persist(item);
-        tem.persist(itemRequest1);
-        tem.persist(itemRequest2);
-        tem.persist(itemRequest3);
-
-        var itemRequests = itemRequestRepository
+        var itemRequestsByRequesterIdNot = itemRequestRepository
                 .findAllByRequesterIdNot(1L, Pageable.unpaged());
-        assertThat(itemRequests.size(), is(1));
+        assertThat(itemRequestsByRequesterIdNot.size(), is(1));
     }
 }
